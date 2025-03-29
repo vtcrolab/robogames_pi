17806595ba94e1f593ec3ec76afa314928ae846b
houndsito <fcampoverdeg@vt.edu>
2025-03-29 14:27:50 -0400

    testing

---
+++ car_robot_ws/src/mini_car_bot/build/mini_car_bot/CMakeFiles/mini_car_bot_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini_car_bot_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/mini_car_bot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
